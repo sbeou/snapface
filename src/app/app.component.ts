b026027ea57763155dd33fa746382b2202d93a92
sbeou13 <design@walf.com.br>
2022-10-23 10:35:17 -0300

    Add directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,58 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit  {
+  faceSnaps!: FaceSnap[];
   mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
   ngOnInit() {
-      this.mySnap = new FaceSnap (
-        'Archibald',
-        'Mon meilleur ami depuis tout petit !',
-        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        new Date(),
-        0
-      )};
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Paris'
+      },
+      {
+        title: 'Alps Montains',
+        description: 'Super photo de montagne',
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/09/02/07/31/alps-7426887_960_720.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Les Alpes'
+      },
+      {
+        title: 'Bee honey',
+        description: 'Jolie petite abeille',
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/09/25/15/16/bee-7478585_960_720.jpg',
+        createdDate: new Date(),
+        snaps: 150
+      },
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Paris'
+      },
+      {
+        title: 'Alps Montains',
+        description: 'Super photo de montagne',
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/09/02/07/31/alps-7426887_960_720.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Les Alpes'
+      },
+      {
+        title: 'Bee honey',
+        description: 'Jolie petite abeille',
+        imageUrl: 'https://cdn.pixabay.com/photo/2022/09/25/15/16/bee-7478585_960_720.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      }
+    ]
+    }
   }
